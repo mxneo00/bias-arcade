6569dc91e4ad07bd1c7687a59e87b2d956a0befd
Katellyn Hyker <llyn00@yahoo.com>
2026-02-17 13:11:27 -0500

    Begin base setup for pages and css files

---
+++ bias-arcade-web/app/(site)/games/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function Games() {
+    return (
+        <div className={styles.page}>
+            <h1>Games</h1>
+        </div>
+    );
+}
